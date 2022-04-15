54982a3ff0ae77fa4c18582f7d04d60fd259c046
Дроздов Семён <s.drozdov2448@yandex.ru>
2022-04-15 16:14:54 +0300

    Задание по dom

--- src/DOM.js
+++ src/DOM.js
@@ -5,6 +5,12 @@
   Считаем, что всегда передается тег, допускающий вставку текста в качестве своего содержимого (P, DIV, I и пр.).
 */
 export function appendToBody(tag, content, count) {
+    let i = 0;
+    for (i = 0; i < count; i++) {
+        let newitem = document.createElement(tag);
+        newitem.innerHTML = content;
+        document.body.appendChild(newitem);
+    }
 }
 
 /*
@@ -15,6 +21,17 @@ export function appendToBody(tag, content, count) {
   Сформированное дерево верните в качестве результата работы функции.
 */
 export function generateTree(childrenCount, level) {
+    function insertNewDiv(currentLevel) {
+        let newdiv = document.createElement('div');
+        newdiv.classList.add(`item_` + currentLevel);
+        if (currentLevel >= level) return newdiv;
+
+        for (let i = 0; i < childrenCount; i++) {
+            newdiv.append(insertNewDiv(currentLevel + 1));
+        }
+        return newdiv;
+    }
+    return insertNewDiv(1);
 }
 
 /*
@@ -26,4 +43,13 @@ export function generateTree(childrenCount, level) {
   Сформированное дерево верните в качестве результата работы функции.
 */
 export function replaceNodes() {
+    let tree = generateTree(2, 3);
+    let node = tree.getElementsByClassName('item_2');
+    for (let elements of node) {
+        let section = document.createElement('section');
+        section.className = 'item_2';
+        section.innerHTML = elements.innerHTML;
+        elements.replaceWith(section);
+    }
+    return tree;
 }
